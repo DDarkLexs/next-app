89ae37c75019d360546b49581bb28708b19d2f05
DDark <antoniolexis2001@outlook.com>
2024-12-14 22:28:32 +0100

    first-commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+function AddToCart() {
+    return (
+        <div>
+            <button onClick={() => console.log('add to card')}>
+                Add to card
+            </button>
+        </div>
+    );
+}
+
+export default AddToCart;
